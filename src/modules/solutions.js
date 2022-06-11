b6ee5f9655f5286965ffed8da2aa73a295905a76
moonkorea00 <moonkorea00@gmail.com>
2022-06-11 03:42:40 -0700

    add : solutions module

--- src/modules/solutions.js
+++ src/modules/solutions.js
@@ -16,13 +16,13 @@ const GET_SOLUTIONS_LOADING = 'GET_SOLUTIONS_LOADING';
 const GET_SOLUTIONS_ERROR = 'GET_SOLUTIONS_ERROR';
 
 // action 생성함수
-export const fetchSolutions = (url, callback) => async dispatch => {
+export const fetchSolutions = (url, cb) => async dispatch => {
   dispatch({ type: GET_SOLUTIONS_LOADING });
   try {
     const res = await fetch(url);
     const data = await res.json();
     dispatch({ type: GET_SOLUTIONS_SUCCESS, data });
-    callback && callback();
+    (cb && cb()) || console.log(`no cb passed`);
   } catch (e) {
     dispatch({ type: GET_SOLUTIONS_ERROR, e });
   }
@@ -33,17 +33,20 @@ export const solutions = (state = initialState, action) => {
   switch (action.type) {
     case GET_SOLUTIONS_SUCCESS:
       return {
-        ...state,
+        loading: false,
         data: action.data,
+        error: null,
       };
     case GET_SOLUTIONS_LOADING:
       return {
-        ...state,
         loading: true,
+        data: null,
+        error: null,
       };
     case GET_SOLUTIONS_ERROR:
       return {
-        ...state,
+        loading: false,
+        data: null,
         error: action.e,
       };
     default:
--- src/pages/WordleGame.js
+++ src/pages/WordleGame.js
@@ -1,20 +1,33 @@
 import styled from 'styled-components';
-import useAsync from '../hooks/useAsync';
+import { fetchSolutions } from '../modules/solutions';
 import Wordle from '../components/Wordle';
 import Loading from '../components/Loading';
 import Error from '../components/Error';
+import { useSelector, useDispatch, shallowEqual } from 'react-redux';
+import { useEffect } from 'react';
 
 const WordleGame = () => {
-  const [state] = useAsync(() => getData());
-  const { data: solution, loading, error } = state;
+  const {
+    loading,
+    data: solution,
+    error,
+  } = useSelector(
+    state => ({
+      loading: state.solutions.loading,
+      data: state.solutions.data,
+      error: state.solutions.error,
+    }),
+    shallowEqual
+  );
+  const dispatch = useDispatch();
 
   const randomSolution =
     solution && solution[Math.floor(Math.random() * solution?.length)].word;
+    
+  useEffect(() => {
+    dispatch(fetchSolutions(`http://localhost:3002/solutions`));
+  }, []);
 
-  const getData = async () => {
-    const res = await fetch(`http://localhost:3002/solutions`);
-    return res.json();
-  };
   if (loading) return <Loading />;
   if (error) return <Error />;
   return (
